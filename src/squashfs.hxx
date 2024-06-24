4847d09eca8329307a4a32f9430cd7a3aa82bd8c
David Hasegawa <david.hasegawa@braincorp.com>
2024-06-24 15:55:29 -0700

    Explain max_supported_file_size in comment

--- src/squashfs.hxx
+++ src/squashfs.hxx
@@ -277,6 +277,11 @@ class MetadataReader
 {
 	MetadataBlockReader f;
 
+	// Supporting the max possible file in squashfs (16 exbibytes) leads to an impossibly large
+	// inode that we'd have to load (16 pebibytes), so we have to pick an arbitrary limit that's
+	// more reasonable.
+	// This doesn't consume physical memory (on Linux) unless that part of the buffer is actually
+	// written to. https://stackoverflow.com/questions/864416/are-some-allocators-lazy
 	static constexpr size_t max_supported_file_size = 1ull << 40; // 1 TiB -> 1 GiB buffer (2^30)
 
 	static constexpr size_t buf_size = squashfs::inode::lreg::max_possible_inode_size(max_supported_file_size);
